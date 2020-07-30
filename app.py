b4331234b1805bc260ec335dc33ca311f090d40e
ZakhrAlex <38586421+ZakhrAlex@users.noreply.github.com>
2020-07-29 23:13:27 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+print('i am planning to learn git conflicts resolution')
+name=input('please insert your name: ')
+surname=input('please insert your surname: ')
+nickname=input('please insert your nuckname: ')
+print('Hello, {} {} a.k.a. {}'.format(name, surname, nickname))
+
